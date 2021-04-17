949c12e359acd677d9de734e291867bd719503d0
Guilherme Fritzen Machado <greer_13@hotmail.com>
2021-04-17 10:55:55 -0300

    fixed the script

--- bmi.js
+++ bmi.js
@@ -7,29 +7,33 @@ function bmi() {
   const results = document.getElementById('results');
 
   const heightCalc = height / 100;
-  const bmiValue = (weight / (heightCalc * heightCalc)).toFixed(1);
+  const bmiValue = weight / (heightCalc * heightCalc);
 
   if (name !== '' && height !== '' && weight !== '') {
     let classification = '';
-    let minRange = 18.5 * (heightCalc * heightCalc).toFixed(1);
-    let maxRange = 24.9 * (heightCalc * heightCalc).toFixed(1);
-    let range = `${minRange} to ${maxRange}`;
+    let minRange = 18.5 * (heightCalc * heightCalc);
+    let maxRange = 24.9 * (heightCalc * heightCalc);
+    let range = `${minRange.toFixed(2)} to ${maxRange.toFixed(2)}`;
 
-    if (bmiValue <= 18.4) {
+    if (bmiValue > 0 && bmiValue <= 18.5) {
       classification = 'Underweight';
-    } else if (bmiValue >= 18.5 && bmiValue <= 24.9) {
+    } else if (bmiValue > 18.5 && bmiValue <= 25) {
       classification = 'Normal';
-    } else if (bmiValue >= 25 && bmiValue <= 29.9) {
+    } else if (bmiValue > 25 && bmiValue <= 29.9) {
       classification = 'Overweight';
     } else {
       classification = 'Obese';
     }
 
     if (classification == 'Normal') {
-      results.textContent = `${name}, Your BMI is ${bmiValue},  ${classification} category for adults of your height.`;
+      results.textContent = `${name}, Your BMI is ${bmiValue.toFixed(
+        2
+      )},  ${classification} category for adults of your height.`;
     } else {
       let fullClassification = ` indicating your weight is in the ${classification}. For your height, a normal weight range would be from ${range} kilograms.`;
-      results.textContent = `${name}, Your BMI is ${bmiValue},  ${fullClassification} `;
+      results.textContent = `${name}, Your BMI is ${bmiValue.toFixed(
+        2
+      )},  ${fullClassification} `;
     }
   } else {
     if (name) {
